8462a13101219a127d2f8dcb921754b364d381f0
Reinier <reinier.v.linschoten@gmail.com>
2024-06-23 12:37:29 +0200

    Moved to version 2.1.0; Prepared testing for getStudyData

--- tests/testthat/test_getStudyData.R
+++ tests/testthat/test_getStudyData.R
@@ -1,10 +1,47 @@
-context("Test getStudyData in all its glory.")
+context("Test if the output of getStudyData matches the expected output.")
 
 creds <- readRDS("testing_credentials.Rds")
 castor_api <- CastorData$new(key=creds$client_id, secret=creds$client_secret, base_url=creds$base_url)
+complete_study_output <- castor_api$getStudyData(creds$output_study)
 
-test_that("getStudyData returns an appropriate object.", {
-  sdf <- castor_api$getStudyData(creds$example_study)
+test_that("getStudyData returns the expected study data.", {
+  actual <- complete_study_output$Study
+  expected <- read.csv("tests/test_files/files_output/CastorStudy.csv", check.names=F)
 
-  expect_equal(creds$study_hash, digest::digest(sdf))
+  expect_identical(actual, expected)
+})
+
+test_that("getStudyData returns the expected data for the repeated measure Unscheduled Visit", {
+  actual <- complete_study_output$`Repeating data`$`Unscheduled visit`
+  expected <- read.csv("tests/test_files/files_output/CastorUnscheduledVisit.csv", check.names=F)
+
+  expect_identical(actual, expected)
+})
+
+test_that("getStudyData returns the expected data for the repeated measure Medication", {
+  actual <- complete_study_output$`Repeating data`$Medication
+  expected <- read.csv("tests/test_files/files_output/CastorMedication.csv", check.names=F)
+
+  expect_identical(actual, expected)
+})
+
+test_that("getStudyData returns the expected data for the repeated measure Comorbidities", {
+  actual <- complete_study_output$`Repeating data`$Comorbidities
+  expected <- read.csv("tests/test_files/files_output/CastorUnscheduledVisit.csv", check.names=F)
+
+  expect_identical(actual, expected)
+})
+
+test_that("getStudyData returns the expected data for the repeated measure Adverse Events", {
+  actual <- complete_study_output$`Repeating data`$`Adverse event`
+  expected <- read.csv("tests/test_files/files_output/CastorAdverseEvent.csv", check.names=F)
+
+  expect_identical(actual, expected)
+})
+
+test_that("getStudyData returns the expected data for the survey Quality of Life", {
+  actual <- complete_study_output$Surveys$`QOL Survey`
+  expected <- read.csv("tests/test_files/files_output/CastorQOLSurvey.csv", check.names=F)
+
+  expect_identical(actual, expected)
 })
